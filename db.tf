8822f7a2080a6fa8c7340bb615a45c225297acce
lipalipinski <lipabass@gmail.com>
2023-08-15 19:03:40 +0200

    db.tf storage.tf

---
+++ db.tf
@@ -0,0 +1,14 @@
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = var.dynamodb_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = var.dynamodb_name
+  }
+}
--- storage.tf
+++ storage.tf
@@ -23,18 +23,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_s3_sse" 
     }
   }
 }
-
-resource "aws_dynamodb_table" "tf_lock" {
-  name         = var.dynamodb_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = var.dynamodb_name
-  }
-}
