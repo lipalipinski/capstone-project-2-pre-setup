98355a50cdc70f75035e180cd714763fe1793cac
lipalipinski <lipabass@gmail.com>
2023-08-16 16:33:53 +0200

    jenkins controller

--- instances.tf
+++ instances.tf
@@ -1,12 +1,51 @@
-module "ec2-instance" {
+module "jenkins-controller" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.2.1"
 
   name = "jenkins-controller"
 
-  instance_type = "t2.micro"
-  subnet_id     = module.vpc.public_subnets[0]
-  vpc_security_group_ids = [module.security-group.security_group_id]
+  instance_type          = "t2.micro"
+  ami                    = "ami-08a52ddb321b32a8c"
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.jenkins-ctrl-sg.id]
 
   associate_public_ip_address = true
+
+  user_data_replace_on_change = true
+  user_data                   = <<EOF
+#!/bin/bash
+yum update -y 
+yum remove -y awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+cat <<EOF1 > /etc/yum.repos.d/adoptium.repo
+[Adoptium]
+name=Adoptium
+baseurl=https://packages.adoptium.net/artifactory/rpm/amazonlinux/2/x86_64                                                        
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.adoptium.net/artifactory/api/gpg/key/public
+EOF1
+yum install -y temurin-17-jdk
+wget -O /etc/yum.repos.d/jenkins.repo \
+  https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum upgrade -y
+yum install -y jenkins
+systemctl daemon-reload
+systemctl enable jenkins
+systemctl start jenkins
+EOF
+}
+
+module "jenkins-worker" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.2.1"
+
+  name = "jenkins-worker"
+
+  instance_type          = "t2.micro"
+  subnet_id              = module.vpc.private_subnets[0]
+  vpc_security_group_ids = [aws_security_group.jenkins-worker-sg.id]
 }
--- security_groups.tf
+++ security_groups.tf
@@ -1,35 +1,65 @@
-module "security-group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
+# CIDR ranges for EC2 connect
+data "aws_ip_ranges" "ec2-connect-ip" {
+  regions  = [var.region]
+  services = ["EC2_INSTANCE_CONNECT"]
+}
 
+resource "aws_security_group" "jenkins-ctrl-sg" {
   name        = "jenkins-ctrl-sg"
   description = "SG for Jenkins controller"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_with_cidr_blocks = [
-    {
-      description = "http 80"
-      cidr_blocks = "0.0.0.0/0"
+  ingress {
+    description = "Allow 22 from EC2 connect"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = data.aws_ip_ranges.ec2-connect-ip.cidr_blocks
+  }
 
-      protocol = 6
-      from_port = 80
-      to_port   = 80
-    },
-    {
-      description = "https 443"
-      cidr_blocks = "0.0.0.0/0"
+  ingress {
+    description = "Allow 8080 from anywhere"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-      protocol = 6
-      from_port = 443
-      to_port   = 443
-    },
-    {
-      description = "ssh 22"
-      cidr_blocks = "0.0.0.0/0"
+  ingress {
+    description = "Allow 443 from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-      protocol = 6
-      from_port = 22
-      to_port   = 22
-    }
-  ]
+  egress {
+    description = "Allow outbound connection"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "jenkins-worker-sg" {
+  name        = "jenkins-worker-sg"
+  description = "SG for Jenkins worker"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description     = "Allow 22 from jenkins-ctrl-sg"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.jenkins-ctrl-sg.id]
+  }
+
+  egress {
+    description = "Allow outbound connection"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
