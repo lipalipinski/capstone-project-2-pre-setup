d1cd380d0754a7e5be26f9ef9f4edd1c605098cb
lipalipinski <lipabass@gmail.com>
2023-08-16 22:50:55 +0200

    install ansible

--- files/jenkins-controller-boot.sh
+++ files/jenkins-controller-boot.sh
@@ -10,3 +10,5 @@ apt-get install -y unzip glibc-source groff less
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-$AWS_CLI_VER.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
+
+apt-get install -y python3-pip ansible
--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "region" {
 
 variable "bucket_name" {
   type    = string
-  default = "terraform-backend-jlpinski-2"
+  default = "terraform-backend-jlpinski-1"
 }
 
 variable "dynamodb_name" {
