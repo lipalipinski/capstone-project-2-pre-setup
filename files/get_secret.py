10f9c1cfd44fc5d0ebbecd6155a181913907ef45
lipalipinski <lipabass@gmail.com>
2023-08-17 15:52:24 +0200

    get ssh key .py script

---
+++ files/get_secret.py
@@ -0,0 +1,36 @@
+import boto3, os, sys
+from botocore.exceptions import ClientError
+
+"""
+Get a secret from AWS Secrets Manager and store it in ~/.ssh/
+"""
+
+secret_name = sys.argv[1]
+
+def get_secret():
+
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    return get_secret_value_response['SecretString']
+
+# save to ~/.ssh/
+f = open(os.path.expanduser(f'~/.ssh/{secret_name}'), "w")
+f.write(get_secret())
+f.close()
