039616ab1f1f1db603fde76bd30524202fb78fce
lipalipinski <lipabass@gmail.com>
2023-08-17 13:52:57 +0200

    .gitignore files/ssh

--- variables.tf
+++ variables.tf
@@ -10,10 +10,10 @@ variable "region" {
 
 variable "bucket_name" {
   type    = string
-  default = "terraform-backend-jlpinski-1"
+  default = "terraform-backend-jlpinski-2"
 }
 
 variable "dynamodb_name" {
   type    = string
-  default = "terraform-lock-jlpinski-1"
+  default = "terraform-lock-jlpinski-2"
 }
