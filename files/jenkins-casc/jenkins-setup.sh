534436eee664a70cd4794fbd85407a32dfc0cf83
lipalipinski <lipabass@gmail.com>
2023-08-21 16:56:55 +0200

    jenkins-setup.sh

--- files/jenkins-casc/jenkins-setup.sh
+++ files/jenkins-casc/jenkins-setup.sh
@@ -2,15 +2,17 @@
 
 # this script installs plugins, jenkins-casc.yaml and restarts jenkins.service
 
+MYDIR="$(dirname "$(readlink -f "$0")")"
+
 # jenkins plugin manager
-java -jar "jenkins-plugin-manager-2.12.13.jar" \
+java -jar "$MYDIR/jenkins-plugin-manager-2.12.13.jar" \
   --war /usr/share/java/jenkins.war \
   --plugin-download-directory "$JENKINS_HOME/plugins" \
-  --plugin-file "jenkins-plugins.yaml"
+  --plugin-file "$MYDIR/jenkins-plugins.yaml"
 chown -R jenkins:jenkins $JENKINS_HOME/plugins/
 
 # jenkins casc yaml
-cp jenkins-casc.yaml $JENKINS_HOME/jenkins.yaml
+cp $MYDIR/jenkins-casc.yaml $JENKINS_HOME/jenkins.yaml
 chown jenkins:jenkins $JENKINS_HOME/jenkins.yaml
 
 # restart jenkins service
