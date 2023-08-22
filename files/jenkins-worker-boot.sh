2cbd0e0f0e0740f6627b4bcb26ebae50875fda56
lipalipinski <lipabass@gmail.com>
2023-08-22 22:46:47 +0200

    apt update jenkins worker install tf

--- files/jenkins-worker-boot.sh
+++ files/jenkins-worker-boot.sh
@@ -44,4 +44,5 @@ sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
 https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 tee /etc/apt/sources.list.d/hashicorp.list
+apt-get update -y 
 apt-get install -y terraform
