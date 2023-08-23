e67b494dc69dae5a42da14f1899271098b2d333c
lipalipinski <lipabass@gmail.com>
2023-08-23 13:47:10 +0200

    jenkins casc Down pipeline

--- files/jenkins-casc/jenkins-setup.sh
+++ files/jenkins-casc/jenkins-setup.sh
@@ -5,6 +5,7 @@
 MYDIR="$(dirname "$(readlink -f "$0")")"
 
 # jenkins plugin manager
+echo "\nInstalling plugins..."
 java -jar "$MYDIR/jenkins-plugin-manager-2.12.13.jar" \
   --war /usr/share/java/jenkins.war \
   --plugin-download-directory "$JENKINS_HOME/plugins" \
@@ -12,8 +13,10 @@ java -jar "$MYDIR/jenkins-plugin-manager-2.12.13.jar" \
 chown -R jenkins:jenkins $JENKINS_HOME/plugins/
 
 # jenkins casc yaml
+echo "\nCopy jenkins.yaml CASC"
 cp $MYDIR/jenkins-casc.yaml $JENKINS_HOME/jenkins.yaml
 chown jenkins:jenkins $JENKINS_HOME/jenkins.yaml
 
 # restart jenkins service
+echo "\nRestart jenkins.service"
 systemctl restart jenkins
