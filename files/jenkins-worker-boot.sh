f2c89d5edacff4758d5a5e04d45ccb9fba72add1
lipalipinski <lipabass@gmail.com>
2023-08-26 13:09:32 +0200

    jenkins worker install docker

--- files/jenkins-worker-boot.sh
+++ files/jenkins-worker-boot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 AWS_CLI_VER="2.13.9"
+DOCKER_VERSION_STRING="5:24.0.5-1~ubuntu.22.04~jammy"
 JENKINS_REMOTE_DIR="/home/ubuntu/jenkins"
 
 # create remote root dir for jenkins
@@ -13,6 +14,8 @@ apt-get update -y && apt-get upgrade -y
 
 apt-get install -y \
   apt-transport-https \
+  ca-certificates \
+  curl \
   glibc-source \
   gnupg \
   groff \
@@ -46,3 +49,27 @@ https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 tee /etc/apt/sources.list.d/hashicorp.list
 apt-get update -y 
 apt-get install -y terraform
+
+# uninstall any old docker docker
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; 
+  do sudo apt-get remove $pkg; 
+done;
+
+# install docker
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg 
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update -y
+
+sudo apt-get install -y \
+  docker-ce=$DOCKER_VERSION_STRING \
+  docker-ce-cli=$DOCKER_VERSION_STRING \
+  containerd.io \
+  docker-buildx-plugin \
+  docker-compose-plugin
