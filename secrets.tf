b78b5b49f0afd38719f90e9e2083c77b15cbb4b0
lipalipinski <lipabass@gmail.com>
2023-09-04 13:17:32 +0200

    jenkins worker executors = 1

--- secrets.tf
+++ secrets.tf
@@ -3,7 +3,7 @@ module "jenkins-worker-private-key" {
   version = "1.1.1"
 
   name          = var.jenkins-worker-pk-name
-  secret_string = file("files/ssh/jenkins-worker-kp")
+  secret_string = file("files/secrets/jenkins-worker-kp")
 
   # setting to 0 enables instant delete
   recovery_window_in_days = 0
@@ -20,7 +20,7 @@ module "app-server-private-key" {
   version = "1.1.1"
 
   name          = "app-server-pk"
-  secret_string = file("files/ssh/app-server-kp")
+  secret_string = file("files/secrets/app-server-kp")
 
   # setting to 0 enables instant delete
   recovery_window_in_days = 0
@@ -29,5 +29,38 @@ module "app-server-private-key" {
     Name                           = "app-server-private-key"
     "jenkins:credentials:type"     = "sshUserPrivateKey"
     "jenkins:credentials:username" = "ubuntu"
+  }
+}
+
+module "jenkins-petclinic-token-user" {
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "1.1.1"
+
+  name          = "jenkins-petclinic-token-user"
+  secret_string = file("files/secrets/gh-token")
+
+  # setting to 0 enables instant delete
+  recovery_window_in_days = 0
+
+  tags = {
+    Name                           = "jenkins-petclinic-token-user"
+    "jenkins:credentials:type"     = "usernamePassword"
+    "jenkins:credentials:username" = "lipalipinski"
+  }
+}
+
+module "jenkins-petclinic-token" {
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "1.1.1"
+
+  name          = "jenkins-petclinic-token"
+  secret_string = file("files/secrets/gh-token")
+
+  # setting to 0 enables instant delete
+  recovery_window_in_days = 0
+
+  tags = {
+    Name                           = "jenkins-petclinic-token"
+    "jenkins:credentials:type"     = "string"
   }
 }
