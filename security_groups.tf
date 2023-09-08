ccff098fdc2b5bdf58220471379662f961bc70c8
lipalipinski <lipabass@gmail.com>
2023-09-08 10:59:10 +0200

    SGs from rds tf

--- security_groups.tf
+++ security_groups.tf
@@ -77,3 +77,61 @@ resource "aws_security_group" "app_lb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "app_server_sg" {
+  name        = "app_server_sg"
+  description = "SG for Petclinic app server"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description     = "Allow 22 from jenkins worker"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.jenkins-worker-sg.id]
+  }
+
+  ingress {
+    description = "Allow 80 from ALB SG and Jenkins worker SG"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [
+      aws_security_group.app_lb_sg.id,
+      aws_security_group.jenkins-worker-sg.id,
+    ]
+  }
+
+  ingress {
+    description = "Allow 8080 from jenkin worker SG"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    security_groups = [
+      aws_security_group.jenkins-worker-sg.id,
+    ]
+  }
+
+  egress {
+    description = "Allow outbound connection"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_security_group" "db-sg" {
+  name        = "db-sg"
+  description = "SG for Petclinic RDS DB"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description     = "Allow access to MYSQL for app SG"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.app_server_sg.id]
+  }
+}
